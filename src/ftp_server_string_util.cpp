8d667324ed56f439060b4a87b520e9f76cc55da1
Ayush Garg <garga@stumail.csci.viu.ca>
2020-11-06 01:35:42 -0800

    string file

--- src/ftp_server_string_util.cpp
+++ src/ftp_server_string_util.cpp
@@ -6,4 +6,65 @@
  *
  */
 
+ #include <string>
+ #include <string.h>
+#include "ftp_server_string_util.hpp"
+ using namespace std;
 
+
+ //Replaces all the occurrences of 'find' character in 'str' with 'replace' character.
+ void replaceAll(char* str, char find, char replace)
+ {
+ 	int l = strlen(str);
+ 	for(int i=0; i<l; i++)
+ 		if(str[i] == find)
+ 			str[i] = replace;
+ }
+
+
+
+ //Returns true if 'str' starts with 'prefix'.
+ bool startsWith(const char* str, const char* prefix)
+ {
+ 	int l = strlen(prefix);
+ 	for(int i=0; i<l; i++)
+ 		if(str[i] != prefix[i])
+ 			return false;
+ 	return true;
+ }
+
+
+ //Returns true if 'str' contains 'substr'
+ bool contains(const char* str, const char* substr)
+ {
+ 	const char *ptr = strstr(str, substr);
+ 	if(ptr != NULL)
+ 		return true;
+ 	return false;
+ }
+
+
+
+ //Changes all characters of 'str' to upper case.
+
+
+ //Removes all the spaces, if there is any, from the beginning and the ending of 'str'.
+ void stripLeadingAndTrailingSpaces(char* str)
+ {
+ 	int count = 0;
+     for (int i = 0; str[i]; i++)
+ 		if (str[i] != ' ')
+ 			str[count++] = str[i];
+     str[count] = '\0';
+ }
+
+
+ //Removes new line character ('\n'), if there is any, from the end of 'str'.
+ void stripNewlineAtEnd(char* str)
+ {
+ 	int count = 0;
+     for (int i = 0; str[i]; i++)
+ 		if (str[i] != '\n')
+ 			str[count++] = str[i];
+     str[count] = '\0';
+ }
