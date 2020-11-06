c2693cc0a34a27b607c2d466ceca894f0600cfbd
Ayush Garg <garga@stumail.csci.viu.ca>
2020-11-06 09:52:32 -0800

    general changes in string

--- src/ftp_server_string_util.cpp
+++ src/ftp_server_string_util.cpp
@@ -22,38 +22,73 @@
  using namespace std;
 
 
- //Replaces all the occurrences of 'find' character in 'str' with 'replace' character.
- void replaceAll(char* str, char find, char replace)
- {
-   string s = str;
-   std::replace(s.begin(), s.end(), find, replace);
 
- }
-
-
-
- //Returns true if 'str' starts with 'prefix'.
- bool startsWith(const char* str, const char* prefix)
- {
- 	int l = strlen(prefix);
- 	for(int i=0; i<l; i++)
+  //Replaces all the occurrences of 'find' character in 'str' with 'replace' character.
+  void replaceAll(char* str, char find, char replace)
   {
- 		if(str[i] != prefix[i])
-    {
- 			return false;
-    }
+    string s = str;
+    std::replace(s.begin(), s.end(), find, replace);
+
   }
- 	return true;
- }
 
 
- //Returns true if 'str' contains 'substr'
- bool contains(const char* str, const char* substr)
- {
- 	const char *ptr = strstr(str, substr);
- 	if(ptr != NULL)
+
+  //Returns true if 'str' starts with 'prefix'.
+  bool startsWith(const char* str, const char* prefix)
   {
- 		return true;
+  	int l = strlen(prefix);
+  	for(int i=0; i<l; i++)
+   {
+  		if(str[i] != prefix[i])
+     {
+  			return false;
+     }
+   }
+  	return true;
   }
- 	return false;
- }
+
+
+  //Returns true if 'str' contains 'substr'
+  bool contains(const char* str, const char* substr)
+  {
+  	const char *ptr = strstr(str, substr);
+  	if(ptr != NULL)
+   {
+  		return true;
+   }
+  	return false;
+  }
+
+
+
+  //Changes all characters of 'str' to upper case.
+  void toUpper(char* str)
+  {
+    string su = str;
+    transform(su.begin(), su.end(), su.begin(), ::toupper);
+  }
+
+
+
+  //Changes all characters of 'str' to lower case.
+  void toLower(char* str)
+  {
+    string su = str;
+    transform(su.begin(), su.end(), su.begin(), ::tolower);
+  }
+
+
+  //Removes all the spaces, if there is any, from the beginning and the ending of 'str'.
+  void stripLeadingAndTrailingSpaces(char* str)
+  {
+    string su = str;
+    su.erase(remove(su.begin(), su.end(), ' '), su.end());
+  }
+
+
+  //Removes new line character ('\n'), if there is any, from the end of 'str'.
+  void stripNewlineAtEnd(char* str)
+  {
+    string su = str;
+    su.erase(remove(su.begin(), su.end(), '\n'), su.end());
+  }
