91014897b122e7956c5311e207144c342e897f3e
Ayush Garg <garga@stumail.csci.viu.ca>
2020-12-03 06:21:19 -0800

    connection listener completed

--- src/ftp_server_connection_listener.cpp
+++ src/ftp_server_connection_listener.cpp
@@ -24,7 +24,6 @@
 
   using namespace std;
 
-
  void startListenerSocket(char* port, int& listenerSockDescriptor, bool& succeded)
  {
 
@@ -41,56 +40,47 @@
  	struct addrinfo* results;
    	int x;
    	x = getaddrinfo(hostname, port, &hints, &results);
+
  	if(x != 0)
  	{
  		cerr<<"Can't get address info."<<endl;
  		exit(1);
  	}
- 	int listenerSocket = -1;
- 	//char* listenerSocketIP = NULL;
+ 	listenerSockDescriptor = -1;
  	struct addrinfo* result = results;
  	while(result != NULL)
  	{
- 		listenerSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
- 		if(listenerSocket != -1)
+ 		listenerSockDescriptor = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+  	if(listenerSockDescriptor != -1)
  		{
- 			int optValue = 1;
- 			if(setsockopt(listenerSocket, SOL_SOCKET, SO_REUSEADDR, &optValue, sizeof(int)) == 0)
+    	int optValue = 1;
+    	if(setsockopt(listenerSockDescriptor, SOL_SOCKET, SO_REUSEADDR, &optValue, sizeof(int)) == 0)
  			{
- 				if(bind(listenerSocket, result->ai_addr, result->ai_addrlen) == 0)
+      	if(bind(listenerSockDescriptor, result->ai_addr, result->ai_addrlen) == 0)
  				{
- 					//struct sockaddr_in* addrinfo_Address = (struct sockaddr_in*)result->ai_addr;
- 				//	listenerSocketIP = inet_ntoa(addrinfo_Address->sin_addr);
- 					break;
+      		break;
  				}
  			}
- 			close(listenerSocket);
- 			listenerSocket = -1;
+ 			close(listenerSockDescriptor);
+ 			listenerSockDescriptor = -1;
  		}
  		result = result->ai_next;
  	}
 
  	freeaddrinfo(results);
- 	if(listenerSocket == -1 || result == NULL)
+ 	if(listenerSockDescriptor == -1 || result == NULL)
  	{
  		cerr<<"Can't open and bind listener socket."<<endl;
  		exit(1);
  	}
  	const int backLog = 5;
- 	listen(listenerSocket, backLog);
- 	listenerSockDescriptor = listenerSocket;
+ 	listen(listenerSockDescriptor, backLog);
  	succeded = true;
 
  }
 
 
 
-
- //Returns true if there is any remote connection request on the listener socket represented by 'sockDescriptor'.
- //Waits for a connection request until 'timeoutSec' + 'timeoutUsec' time.
- //If no connection request has been received before the time out, sets 'isTimedout' value to 'true'.
- //If any error occurs, sets 'isError' value to 'true'.
- //Calls 'isSocketReadyToRead()' function from 'ftp_server_net_util.hpp' to do all of the above.
 
  bool isListenerSocketReady(const int sockDescriptor, const int timeoutSec, const int timeoutUSec, bool& isError, bool&isTimedout)
  {
@@ -101,9 +91,7 @@
 
 
 
- //Accepts a connection request on the listener socket represented by 'listenerSockDescriptor'.
- //Opens the connection to the remote computer and represents the connection by 'clientSockDescriptor'.
- void acceptClientConnection(const int listenerSockDescriptor, int& clientSockDescriptor)
+void acceptClientConnection(const int listenerSockDescriptor, int& clientSockDescriptor)
  {
  	struct sockaddr_in connectionAddress;
  	socklen_t socklen = (socklen_t) sizeof(struct sockaddr_in);
@@ -120,7 +108,6 @@
 
 
 
- //Closes the listener socket represented by 'listenerSockDescriptor'.
  void closeListenerSocket(int& listenerSockDescriptor)
  {
  	close(listenerSockDescriptor);
